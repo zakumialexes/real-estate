fac1abff5ca9f848463a7e57f6903d267d44aacf
username <kiritokirikaya80@gmail.com>
2022-11-19 16:26:33 +0630

    Layout:Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-import logo from "./logo.svg"
-import "./App.css"
+import { useMediaQuery, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Banner, Footer, Header, Navbar } from "./layout/layout";
+import { Sidebar } from "./layout/navbar/navbar";
 
 function App() {
-    return <div className="App">Shwe Real Estate</div>
+  const [openSidebar, setOpenSidebar] = useState(false);
+  const style = {
+    marginLeft: "45vw",
+    width: "100vw",
+    overflow: "hidden",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    transition: "all .5s",
+  };
+  const theme = useTheme();
+  const large = useMediaQuery(theme.breakpoints.up("lg"));
+  const medium = useMediaQuery(theme.breakpoints.down(991));
+  useEffect(() => {
+    if (!medium) {
+      setOpenSidebar(false);
+    }
+  }, [medium]);
+  return (
+    <>
+      {openSidebar === true && medium ? (
+        <Sidebar openSidebar={openSidebar} />
+      ) : null}
+      <Box
+        sx={openSidebar ? { ...style } : null}
+        onClick={() => {
+          if (openSidebar === true) {
+            setOpenSidebar(false);
+          }
+        }}
+      >
+        <Navbar
+          large={large}
+          medium={medium}
+          openSidebar={openSidebar}
+          setOpenSidebar={setOpenSidebar}
+        />
+        <Header />
+        <Banner />
+        <Footer />
+      </Box>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/layout/layout.jsx
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./navbar/navbar";
+export { default as Footer } from "./footer/footer";
+export { default as Header } from "./header/header";
+export { default as Banner } from "./banner/banner";
