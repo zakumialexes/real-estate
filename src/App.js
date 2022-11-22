1309bf4a932ea2c576d3f76bf71825ceea2126a6
zakumi <zakumialex@gmail.com>
2022-11-22 18:13:43 +0630

    ADD: Layout and Router

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
-import { useMediaQuery, useTheme } from "@mui/material";
-import { Box } from "@mui/system";
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Banner, Footer, Header, Navbar } from "./layout/layout";
-import { Sidebar } from "./layout/navbar/navbar";
+import "./App.css"
+import { RouterProvider } from "react-router-dom"
+import router from "./router/route"
+import { Layout } from "./layout/layout"
 
 function App() {
-  const [openSidebar, setOpenSidebar] = useState(false);
-  const style = {
-    marginLeft: "65vw",
-    width: "100vw",
-    overflow: "hidden",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    transition: "all .5s",
-  };
-  const theme = useTheme();
-  const large = useMediaQuery(theme.breakpoints.up("lg"));
-  const medium = useMediaQuery(theme.breakpoints.down(991));
-  useEffect(() => {
-    if (!medium) {
-      setOpenSidebar(false);
-    }
-  }, [medium]);
-  return (
-    <>
-      {openSidebar === true && medium ? (
-        <Sidebar openSidebar={openSidebar} />
-      ) : null}
-      <Box
-        sx={openSidebar ? { ...style } : null}
-        onClick={() => {
-          if (openSidebar === true) {
-            setOpenSidebar(false);
-          }
-        }}
-      >
-        <Navbar
-          large={large}
-          medium={medium}
-          openSidebar={openSidebar}
-          setOpenSidebar={setOpenSidebar}
-        />
-        <Header />
-        <Banner />
-        <Footer />
-      </Box>
-    </>
-  );
+    return (
+        <Layout>
+            <RouterProvider router={router} />
+        </Layout>
+    )
 }
 
-export default App;
+export default App
