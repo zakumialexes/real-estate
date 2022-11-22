8f9c07fabfd5843949a77fd712961a72b6bd0136
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-22 20:00:08 +0630

    Fixed: carousel error in featured-properties.jsx

--- src/pages/agent-list/right-side/featured-properties/featured-properties.jsx
+++ src/pages/agent-list/right-side/featured-properties/featured-properties.jsx
@@ -3,6 +3,8 @@ import React, {useEffect, useRef, useState} from "react";
 import Slider from "react-slick"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowLeft, faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function FeaturedProperties() {
     const [houses, setHouses] = useState([])
