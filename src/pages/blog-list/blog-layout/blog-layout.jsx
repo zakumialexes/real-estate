b39b208982c720cce2f0f687207497ab4c6afcd3
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-11-24 16:09:47 +0630

    Modified: extracted blog list into layout

---
+++ src/pages/blog-list/blog-layout/blog-layout.jsx
@@ -0,0 +1,37 @@
+import {Container, Grid} from "@mui/material";
+import style from "./blog-layout.module.scss";
+import CategoriesProperties from "../../house-list/categories-properties/categories-properties";
+import FeaturedProperties from "../../agent-list/right-side/featured-properties/featured-properties";
+import TagsList from "../tags/tags-list";
+
+function BlogLayout({children}) {
+    return (
+        <Container maxWidth="md" sx={{padding: "60px 0"}}>
+            <div className={style.breadcrumbContainer}>
+                <ol className={style.breadcrumb}>
+                    <li className="">
+                        <a href="src/pages/blog-list/blog-layout/blog-layout#">Home</a>
+                    </li>
+                    <li className="" aria-current="page">
+                        Simple Listing –
+                    </li>
+                </ol>
+                <h2 className={style.breadcrumbTitle}>Blog</h2>
+            </div>
+            <Grid container direction="row" justifyContent="center" spacing={4}>
+                {/*Left Side*/}
+                <Grid item container direction="column" md={8}>
+                    {children}
+                </Grid>
+                {/*Right Side*/}
+                <Grid item direction="column" md={4} maxWidth={"100%"}>
+                    <CategoriesProperties/>
+                    <FeaturedProperties/>
+                    <TagsList/>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
+
+export default BlogLayout;
--- src/pages/blog-list/blog-list.jsx
+++ src/pages/blog-list/blog-list.jsx
@@ -1,5 +1,4 @@
-import style from "./blog-list.module.scss"
-import {Container, Grid, PaginationItem} from "@mui/material";
+import {Grid, PaginationItem} from "@mui/material";
 import {CustomPagination, IconTextField} from "../agent-list/custom-components/custom-components";
 import {useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -7,9 +6,7 @@ import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import usePaginate from "../agent-list/pagination";
 import BlogCard from "./blog-card/blog-card";
 import {ArrowBackOutlined, ArrowForwardOutlined} from "@mui/icons-material";
-import CategoriesProperties from "../house-list/categories-properties/categories-properties";
-import FeaturedProperties from "../agent-list/right-side/featured-properties/featured-properties";
-import TagsList from "./tags/tags-list";
+import BlogLayout from "./blog-layout/blog-layout";
 
 export default function BlogList() {
     const [paginationPage, setPaginationPage] = useState(1);
@@ -26,49 +23,27 @@ export default function BlogList() {
     }
 
     return (
-        <Container maxWidth="md" sx={{padding: "60px 0"}}>
-            <div className={style.breadcrumbContainer}>
-                <ol className={style.breadcrumb}>
-                    <li className="">
-                        <a href="#">Home</a>
-                    </li>
-                    <li className="" aria-current="page">
-                        Simple Listing –
-                    </li>
-                </ol>
-                <h2 className={style.breadcrumbTitle}>Blog</h2>
-            </div>
-            <Grid container direction="row" justifyContent="center" spacing={4}>
-                {/*Left Side*/}
-                <Grid item container direction="column" md={8}>
-                    <Grid item>
-                        <IconTextField value={searchKeyword} handleChange={handleSearchKeyword}
-                                       forName="Search Keyword" icon={<FontAwesomeIcon icon={faMagnifyingGlass}/>}/>
-                    </Grid>
-                    <Grid item container>
-                        {paginatedBlogs.map((blog, i) => (
-                            <BlogCard blog={blog} key={i}/>
-                        ))}
-                    </Grid>
-                    <Grid item display="flex" justifyContent="center">
-                        <CustomPagination page={paginationPage} size="large" count={totalPageCount} variant="outlined"
-                                          renderItem={(item) => (
-                                              <PaginationItem
-                                                  slots={{
-                                                      previous: ArrowBackOutlined,
-                                                      next: ArrowForwardOutlined
-                                                  }} {...item}
-                                              />
-                                          )} onChange={handlePaginationChange}/>
-                    </Grid>
-                </Grid>
-                {/*Right Side*/}
-                <Grid item direction="column" md={4} maxWidth={"100%"}>
-                    <CategoriesProperties/>
-                    <FeaturedProperties/>
-                    <TagsList/>
-                </Grid>
+        <BlogLayout>
+            <Grid item>
+                <IconTextField value={searchKeyword} handleChange={handleSearchKeyword}
+                               forName="Search Keyword" icon={<FontAwesomeIcon icon={faMagnifyingGlass}/>}/>
             </Grid>
-        </Container>
+            <Grid item container>
+                {paginatedBlogs.map((blog, i) => (
+                    <BlogCard blog={blog} key={i}/>
+                ))}
+            </Grid>
+            <Grid item display="flex" justifyContent="center">
+                <CustomPagination page={paginationPage} size="large" count={totalPageCount} variant="outlined"
+                                  renderItem={(item) => (
+                                      <PaginationItem
+                                          slots={{
+                                              previous: ArrowBackOutlined,
+                                              next: ArrowForwardOutlined
+                                          }} {...item}
+                                      />
+                                  )} onChange={handlePaginationChange}/>
+            </Grid>
+        </BlogLayout>
     )
 }
