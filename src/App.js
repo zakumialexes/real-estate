2e14ea4f002fe96a24a793f43ebead8d3b9a94a9
zawmyoniang <zzawmyonaing001@gmail.com>
2022-11-25 00:48:44 +0630

    Update: About page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from "./logo.svg"
+import {About} from "./page/about/about"
 import "./App.css"
+import { Carousel } from "./page/about/carousel"
 
 function App() {
-    return <div className="App">Shwe Real Estate</div>
+    return <div className="App"><About/></div>
 }
 
 export default App
