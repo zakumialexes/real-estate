46df384d87b9f90a294e92b55e030d019b04e09f
zakumi <zakumialex@gmail.com>
2022-11-25 17:29:37 +0630

    ADD:Comment Section

--- src/pages/blog-single/blog-single.jsx
+++ src/pages/blog-single/blog-single.jsx
@@ -1,33 +1,34 @@
-import React, {useEffect, useState} from 'react';
-import BlogLayout from "../blog-list/blog-layout/blog-layout";
+import React, { useEffect, useState } from "react"
+import BlogLayout from "../blog-list/blog-layout/blog-layout"
 import style from "./blog-single.module.scss"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
     faCalendarDays,
     faChevronCircleLeft,
     faChevronCircleRight,
-    faQuoteLeft
-} from "@fortawesome/free-solid-svg-icons";
-import {faComment, faEye} from "@fortawesome/free-regular-svg-icons";
-import {faFacebookF, faGoogle, faLinkedin, faPinterest, faTwitter} from "@fortawesome/free-brands-svg-icons";
-import RelatedBlogCard from "./related-post-card/related-blog-card";
-import {Grid, Typography} from "@mui/material";
+    faQuoteLeft,
+} from "@fortawesome/free-solid-svg-icons"
+import { faComment, faEye } from "@fortawesome/free-regular-svg-icons"
+import { faFacebookF, faGoogle, faLinkedin, faPinterest, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import RelatedBlogCard from "./related-post-card/related-blog-card"
+import { Grid, Typography } from "@mui/material"
+import CommentSection from "./comment-section/comment-section"
 
 function BlogSingle() {
-    const [blog, setBlog] = useState();
+    const [blog, setBlog] = useState()
     const [relatedBlogs, setRelatedBlogs] = useState([])
 
     async function fetchBlogs() {
         const [fetchedBlog, fetchedRelatedBlog] = await Promise.all([
-            fetch("http://localhost:3500/blog-list/1").then(response => response.json()),
-            fetch("http://localhost:3500/blog-list?_limit=2").then(response => response.json())
+            fetch("http://localhost:3500/blog-list/1").then((response) => response.json()),
+            fetch("http://localhost:3500/blog-list?_limit=2").then((response) => response.json()),
         ])
         setBlog(fetchedBlog)
         setRelatedBlogs(fetchedRelatedBlog)
     }
 
     useEffect(() => {
-        fetchBlogs();
+        fetchBlogs()
     }, [])
 
     return (
@@ -35,53 +36,65 @@ function BlogSingle() {
             <div className={style.blogSingleContent}>
                 <div className={style.tagContainer}>
                     {blog?.tags.map((tag, i) => (
-                        <a href="#" className={style.tag} key={i}>{tag}</a>
+                        <a href="#" className={style.tag} key={i}>
+                            {tag}
+                        </a>
                     ))}
                 </div>
                 <h3 className={style.blogTitle}>{blog?.title}</h3>
                 <div className={style.blogInfo}>
-                    <img src={blog?.creator.photo} alt=""/>
+                    <img src={blog?.creator.photo} alt="" />
                     <p>{blog?.creator.name}</p>
-                    <p><FontAwesomeIcon icon={faCalendarDays}/> {blog?.createdDate}</p>
-                    <p><FontAwesomeIcon icon={faEye}/> 123</p>
-                    <p><FontAwesomeIcon icon={faComment}/> 123</p>
+                    <p>
+                        <FontAwesomeIcon icon={faCalendarDays} /> {blog?.createdDate}
+                    </p>
+                    <p>
+                        <FontAwesomeIcon icon={faEye} /> 123
+                    </p>
+                    <p>
+                        <FontAwesomeIcon icon={faComment} /> 123
+                    </p>
                 </div>
                 <div className={style.imgContainer}>
-                    <img src={blog?.photo} alt=""/>
+                    <img src={blog?.photo} alt="" />
                 </div>
                 <div className={style.description}>
                     <p>{blog?.description}</p>
                 </div>
                 <div className={style.quoteContainer}>
                     <div className={style.blockQuote}>
-                        <span className={style.quoteIcon}><FontAwesomeIcon icon={faQuoteLeft}/></span><br/>
-                        <em className={style.quoteText}>Duis mollis et sem sed sollicitudin. Donec non odio
-                            neque. Aliquam hendrerit sollicitudin purus, quis
-                            rutrum mi accumsan nec.</em>
+                        <span className={style.quoteIcon}>
+                            <FontAwesomeIcon icon={faQuoteLeft} />
+                        </span>
+                        <br />
+                        <em className={style.quoteText}>
+                            Duis mollis et sem sed sollicitudin. Donec non odio neque. Aliquam hendrerit sollicitudin
+                            purus, quis rutrum mi accumsan nec.
+                        </em>
                     </div>
                 </div>
                 <div className={style.socials}>
                     <p>Share</p>
                     <div className={style.socialIcon}>
-                        <FontAwesomeIcon icon={faFacebookF}/>
+                        <FontAwesomeIcon icon={faFacebookF} />
                     </div>
                     <div className={style.socialIcon}>
-                        <FontAwesomeIcon icon={faTwitter}/>
+                        <FontAwesomeIcon icon={faTwitter} />
                     </div>
                     <div className={style.socialIcon}>
-                        <FontAwesomeIcon icon={faLinkedin}/>
+                        <FontAwesomeIcon icon={faLinkedin} />
                     </div>
                     <div className={style.socialIcon}>
-                        <FontAwesomeIcon icon={faGoogle}/>
+                        <FontAwesomeIcon icon={faGoogle} />
                     </div>
                     <div className={style.socialIcon}>
-                        <FontAwesomeIcon icon={faPinterest}/>
+                        <FontAwesomeIcon icon={faPinterest} />
                     </div>
                 </div>
                 <div className={style.blogPaginate}>
                     <div className={style.prevPage}>
                         <a href="#">
-                            <FontAwesomeIcon icon={faChevronCircleLeft}/>
+                            <FontAwesomeIcon icon={faChevronCircleLeft} />
                         </a>
                         <div className={style.blogPaginateContent}>
                             <h5>Previous Post</h5>
@@ -94,7 +107,7 @@ function BlogSingle() {
                             <p>Housing Marketing That</p>
                         </div>
                         <a href="#">
-                            <FontAwesomeIcon icon={faChevronCircleRight}/>
+                            <FontAwesomeIcon icon={faChevronCircleRight} />
                         </a>
                     </div>
                 </div>
@@ -104,13 +117,14 @@ function BlogSingle() {
                     </Grid>
                     {relatedBlogs.map((relatedBlog, i) => (
                         <Grid item key={i} sm={6}>
-                            <RelatedBlogCard blog={relatedBlog}/>
+                            <RelatedBlogCard blog={relatedBlog} />
                         </Grid>
                     ))}
                 </Grid>
+                <CommentSection />
             </div>
         </BlogLayout>
-    );
+    )
 }
 
-export default BlogSingle;
+export default BlogSingle
---
+++ src/pages/blog-single/comment-section/comment-section.jsx
@@ -0,0 +1,210 @@
+import { useState } from "react"
+import {
+    Grid,
+    Paper,
+    Typography,
+    Avatar,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemAvatar,
+    Box,
+    TextField,
+    Rating,
+    Button,
+} from "@mui/material"
+
+export default function CommentSection() {
+    const [totalState, setTotalState] = useState({
+        rating: 4.5,
+        reviews: 256,
+    })
+    const [reviews, setReviews] = useState([
+        {
+            id: "1",
+            username: "Alexa",
+            date: "December 20, 2022",
+            review: `I have been comparing this house with others in different aspects like
+            enviroment, neighbourhood, and others.And this one is 8 out of 10.I
+            definitely recommand it.`,
+            rating: 4,
+        },
+    ])
+    const submitReview = () => {
+        const date = new Date()
+        const months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ]
+        // axios.post("http://localhost:3500",{commentState,date:`{date.getDate()} {months[date.getMonth() - 1]},{date.getFullYear()}`})
+    }
+    const [commentState, setCommentState] = useState({
+        username: "",
+        review: "",
+        rating: 4,
+    })
+    return (
+        <Box sx={{ marginTop: "20px" }}>
+            <Paper sx={{ padding: "25px 25px 15px 25px" }}>
+                <Grid container>
+                    <Grid
+                        display="inline-grid"
+                        gridTemplateColumns="max-content max-content max-content"
+                        item
+                        xs={8}
+                        columnGap="4px"
+                        gridAutoRows="1fr"
+                        rowGap="4px"
+                    >
+                        <Grid container alignItems="center">
+                            <Typography variant="h2" fontSize="18px" fontWeight="bold" color="text.main">
+                                {totalState.reviews} Reviews
+                            </Typography>
+                        </Grid>
+                        <Grid container gap="2px" alignItems="center">
+                            <Rating name="read-only" defaultValue={4.5} readOnly />
+                        </Grid>
+                        <Grid item sx={{ opacity: 0.5 }} height="100%">
+                            ( {totalState.rating} out of 5 )
+                        </Grid>
+                    </Grid>
+                    <Grid
+                        container
+                        item
+                        xs={0}
+                        sx={{ display: { xs: "none", sm: "block" } }}
+                        justifyContent="flex-end"
+                        color="#ff0000"
+                    >
+                        Write a Review
+                    </Grid>
+                </Grid>
+                <Grid>
+                    <List>
+                        {reviews.map((review) => (
+                            <ListItem>
+                                <ListItemAvatar
+                                    key={review.id}
+                                    sx={{ width: "90px", height: "90px", display: "flex", alignItems: "center" }}
+                                >
+                                    <Avatar
+                                        alt="Profile Picture"
+                                        src="images/agents/agent-1.jpg"
+                                        sx={{ width: "70px", height: "70px" }}
+                                    />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    disableTypography
+                                    primary={
+                                        <Grid display="grid" gridTemplateColumns="max-content max-content">
+                                            <Typography
+                                                color="text.main"
+                                                fontWeight="bold"
+                                                fontSize="18px"
+                                                display="inline-flex"
+                                                alignItems="flex-end"
+                                            >
+                                                {review.username}
+                                            </Typography>
+                                            <Typography
+                                                display="inline-flex"
+                                                alignItems="center"
+                                                gap="5px"
+                                                marginLeft="8px"
+                                            >
+                                                <Rating name="read-only2" value={review.rating} readOnly />
+                                            </Typography>
+                                        </Grid>
+                                    }
+                                    secondary={
+                                        <>
+                                            <Typography fontSize="14px" margin="8px 0 12px 0" lineHeight="1.2rem">
+                                                {review.date}
+                                            </Typography>
+
+                                            <Typography fontSize="14px" lineHeight="1.2rem">
+                                                {review.review}
+                                            </Typography>
+                                        </>
+                                    }
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Grid>
+            </Paper>
+            <Paper sx={{ padding: "25px", marginTop: "20px" }}>
+                <Typography color="text.main" fontWeight="bold">
+                    Write a Review
+                </Typography>
+                <Grid display="inline-grid" gridTemplateColumns="max-content max-content" margin="30px 0">
+                    <Rating
+                        name="read-only3"
+                        value={commentState.rating}
+                        onChange={(e) => setCommentState({ ...commentState, rating: e.target.value })}
+                        precision={0.5}
+                    />
+                    <Typography
+                        display="inline-flex"
+                        alignItems="center"
+                        fontWeight="350"
+                        fontSize="14px"
+                        color="text.main"
+                        marginLeft="30px"
+                    >
+                        Your Rating & Review
+                    </Typography>
+                </Grid>
+                <TextField
+                    type="text"
+                    placeholder="Your Name"
+                    value={commentState.username}
+                    onChange={(e) => setCommentState({ ...commentState, title: e.target.value })}
+                    fullWidth
+                />
+                <TextField
+                    fullWidth
+                    sx={{
+                        marginTop: "20px",
+                        height: "200px",
+                    }}
+                    inputProps={{ style: { height: "100%" } }}
+                    placeholder="Your Review"
+                    value={commentState.review}
+                    onChange={(e) => setCommentState({ ...commentState, review: e.target.value })}
+                    multiline
+                    rows={6}
+                />
+                <Button
+                    color="reviewSubmitButton"
+                    variant="contained"
+                    sx={{
+                        boxShadow: "none",
+                        borderRadius: "8px",
+                        padding: { xs: "10px", sm: "15px" },
+                        border: "2px solid #ff5a5f",
+                        "&:hover": {
+                            backgroundColor: "#fff",
+                            color: "#ff5a5f",
+                            boxShadow: "none",
+                        },
+                    }}
+                    onClick={submitReview}
+                >
+                    {" "}
+                    Submit Review
+                </Button>
+            </Paper>
+        </Box>
+    )
+}
